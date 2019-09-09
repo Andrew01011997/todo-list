f055d5e6f11ab456bdb522873beb5e7e34f2b1f4
Andrew <Andrew01.01.1997@gmail.com>
2019-09-09 15:18:34 +0300

    Updates

---
+++ precache-manifest.3302e2a96d877510e7a9c2bd1e77b33e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b80527d90e1cd04ca3aa154909f284",
+    "url": "/Andrew01011997/todo-list.git/index.html"
+  },
+  {
+    "revision": "4a294bda83aba39ccce8",
+    "url": "/Andrew01011997/todo-list.git/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "4a294bda83aba39ccce8",
+    "url": "/Andrew01011997/todo-list.git/static/js/2.07c0cde6.chunk.js"
+  },
+  {
+    "revision": "af74211f2d179b3ee710",
+    "url": "/Andrew01011997/todo-list.git/static/js/main.9cfc4188.chunk.js"
+  },
+  {
+    "revision": "d7021c8b3f16c9396613",
+    "url": "/Andrew01011997/todo-list.git/static/js/runtime~main.0f9b069b.js"
+  }
+]);
